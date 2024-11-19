122b8783bf41665542c825366a458270dd80ecff
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2024-11-19 08:07:53 -0500

    Add files via upload

---
+++ conceseionariofx/src/main/java/co/edu/uniquindio/poo/controller/ConsultaTransaccionController.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.poo.controller;
+
+import co.edu.uniquindio.poo.model.Concesionario;
+
+public class ConsultaTransaccionController {
+        Concesionario concesionario;
+
+    public ConsultaTransaccionController(Concesionario concesionario) {
+        this.concesionario = concesionario;
+    }
+}
